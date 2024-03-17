6c8f7e1a1da568fc030a6b9efb8965f11e55360c
PREETHAM <preetham0802@gmail.com>
2024-03-17 23:40:56 +0530

    first commit

---
+++ VIGNAN_LOGO.py
@@ -0,0 +1,142 @@
+from turtle import *
+import turtle
+from time import sleep
+t = turtle.Turtle()
+
+pensize(1)
+bgcolor("lightblue")
+color("black")
+penup()
+goto(-200, 210)
+pendown()
+for j in range(4):
+    fd(350)
+    for i in range(9):
+        rt(10)
+        fd(10)
+# Starting position of the star outline
+penup()
+end_fill()
+goto(-139, 50)
+rt(40)  # Sets the angle of the outline of the star
+lt(3)
+fd(100)
+rt(92)
+# First curve at the bottom left side
+for i in range(5):
+    lt(10)
+    fd(8)
+pendown()
+fillcolor("#9900ff")
+begin_fill()
+rt(45)
+lt(9)
+fd(100)
+lt(90)
+fd(26)
+lt(90)
+fd(90)
+rt(92)
+# Second curve at the bottom right side
+for i in range(4):
+    lt(10)
+    fd(8)
+rt(70)
+fd(90)
+lt(90)
+fd(26)
+lt(90)
+fd(90)
+rt(92)
+lt(10)
+# Third curve at the top right side
+for i in range(5):
+    lt(10)
+    fd(8)
+rt(70)
+fd(90)
+lt(90)
+fd(26)
+lt(90)
+fd(90)
+rt(92)
+lt(10)
+# Fourth curve at the top side
+for i in range(4):
+    lt(10)
+    fd(8)
+rt(70)
+fd(90)
+lt(90)
+fd(26)
+lt(90)
+fd(90)
+# F
+rt(92)
+lt(10)
+# Fifth curve at the top side
+for i in range(5):
+    lt(10)
+    fd(8)
+rt(70)
+fd(90)
+lt(90)
+fd(26)
+lt(90)
+fd(90)
+rt(92)
+lt(10)
+for i in range(5):
+    lt(10)
+    fd(6)
+end_fill()
+# Outline of inside outer circle
+penup()
+rt(4)
+lt(25)
+fd(21)
+pendown()
+fillcolor('white')
+begin_fill()
+circle(40)
+end_fill()
+
+
+hideturtle()
+t.hideturtle()
+turtle.done()
+
+
+''' lt(90)
+fd(30)
+lt(90)
+fd(110)
+lt(90) '''
+
+
+''' # Top semi-circle
+# penup()
+# goto(-139, 90)
+# lt(3)
+# pendown()
+# fillcolor("#9900ff")
+# begin_fill()
+# pencolor("#9900ff")
+# lt(75)
+# fd(90)
+# lt(107)
+# fd(10)
+# for i in range(3):
+#     lt(10)
+#     fd(20)
+#     lt(10)
+#     fd(25)
+# lt(10)
+# fd(10)
+# end_fill()'''
+
+pencolor()  # Used to change the color of the pen used while drawing
+begin_fill()  # Used to inform the robot to start filling the color ; Without end_fill() the begin_fill() will not work
+end_fill()  # Used to inform the robot to stop filling the color ; Without begin_fill() the end_fill() will not work
+pensize()  # Used to inform the robot to that what kind of pencil should be used
+bgcolor()  # Used to set the background of the turtle window
